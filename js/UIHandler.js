bf2a639dbfe53a02c98e642f9169257c102a763a
Barış <46918905+Baris-Yildiz@users.noreply.github.com>
2024-12-06 16:19:35 +0300

    ui iyileştirmesi

--- js/UIHandler.js
+++ js/UIHandler.js
@@ -1,3 +1,48 @@
+let buttons = {
+    ABOUT_BUTTON:0,
+    OPTIONS_BUTTON:1,
+    KEYBINDINGS_BUTTON:2
+};
+
+let quality = ["LOW", "MEDIUM", "HIGH"];
+
+function onSettingsSliderValueChanged(id) {
+    let labels = document.getElementsByTagName('label');
+    let slider = document.getElementById(id);
+    for( let i = 0; i < labels.length; i++ ) {
+        if (labels[i].htmlFor === id){
+            if (id === "Texture Quality" || id === "Environment Quality") {
+                labels[i].innerText = id + ": " + quality[slider.value];
+            } else {
+                labels[i].innerText = id + ": " + slider.value;
+            }
+
+            break;
+        }
+    }
+}
+
+function onButtonClick(clickedButton) {
+    let uiPages = [
+        document.getElementById("aboutPage"),
+        document.getElementById("optionsPage"),
+        document.getElementById("keybindingPage"),
+    ];
+
+    for (let i = 0; i < uiPages.length; i++) {
+        uiPages[i].style.display = "none";
+    }
+
+    let uiButtons = document.getElementsByClassName("navBarButton");
+
+    for (let i = 0; i < uiButtons.length; i++) {
+        uiButtons.item(i).classList.remove("navBarButtonClicked");
+    }
+
+    uiButtons.item(clickedButton).classList.add("navBarButtonClicked");
+    uiPages[clickedButton].style.display = "block";
+}
+
 class UIHandler {
     constructor() {
         this.gameUI = document.getElementById("gameUI");
@@ -8,7 +53,6 @@ class UIHandler {
             if (key !== "h") {
                 return;
             }
-            console.log("s");
             this.uiState = !this.uiState;
             this.gameUI.style.visibility = this.uiState ? "unset" : "hidden";
         };
@@ -23,13 +67,13 @@ class UIHandler {
     }
 
     CreateHelpMenu() {
-        this.AppendUIText("HELP MENU");
+        /*this.AppendUIText("HELP MENU");
         this.AppendUIText("-----------------");
 
         this.AppendUIText("CONTROLS");
         this.AppendUIText("-----------------");
 
         this.AppendUIText("SHORTCUTS");
-        this.AppendUIText("-----------------");
+        this.AppendUIText("-----------------");*/
     }
 }
