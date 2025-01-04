d2a8ddc30776e9b385c968a4349323a80dfb9dca
Barış <46918905+Baris-Yildiz@users.noreply.github.com>
2025-01-04 11:38:35 +0300

    tüm dosyalar vite klasörüne konuldu

--- vite-project/js/UIHandler.js
+++ vite-project/js/UIHandler.js
@@ -88,12 +88,12 @@ function initUI(game) {
     gameRef = game;
     gameUI = document.getElementById("gameUI");
     uiState = false;
-    document.addEventListener("keyup",toggleUI);
+    document.addEventListener("keyup",toggleUI);/*
     game.backgroundMusic.volume = game.settings.music;
     game.backgroundMusic.loop = true;
     game.backgroundMusic.play().catch(error => {
         console.error('Music playback failed:', error);
-    });
+    });*/
 }
 
 
